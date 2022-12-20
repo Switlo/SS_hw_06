9a38fdcf39dd280f7bda2819764dfd19e7437ef3
Svitlana R <sv73@ukr.net>
2022-12-20 11:58:33 +0200

    Added date

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ function currentTime() {
   min = updateTime(min);
   sec = updateTime(sec);
 
+  let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "Desember",
+];
+let currentMonth = date.getMonth();
+let month = months[currentMonth];
+let day = date.getDate();
+let year = date.getFullYear();
+  
+  month = updateTime(month);
+  day = updateTime(day);
+  year = updateTime(year);
+ 
 function updateTime(k) {  // додаємо нуль у числах з однією цифрою
   if (k < 10) {
     return "0" + k;
@@ -18,8 +41,9 @@ function updateTime(k) {  // додаємо нуль у числах з одні
     return k;
   }
 }
-
-document.querySelector("#clock").innerHTML = hour + ` : ` + min + ` : ` + sec;
+  document.querySelector("#monthDay").innerHTML = `${month}, ${day}`;
+  document.querySelector("#hours").innerHTML = `${hour}:${min}:${sec}`;
+  document.querySelector("#year").innerHTML = year;
 
   let t = setTimeout(function(){ currentTime() }, 1000); // налаштування таймеру
 }
